33e0318029d2cd4f019f9d68c74a443a2c3d7877
Влад Карпич <vk0204111@gmail.com>
2023-10-01 17:35:10 +0100

    first

---
+++ scripts/id.js
@@ -0,0 +1,112 @@
+//Кнопки Header and Buttom Header
+
+// Получаем кнопку по ID
+var Katalog = document.getElementById("Katalog");
+// Добавляем обработчик события для кнопки
+Katalog.addEventListener("click", function() {
+  // Ваш код обработки события
+  alert("Каталог");
+});
+
+var ArtiZenLogo = document.getElementById("ArtiZenLogo");
+ArtiZenLogo.addEventListener("click", function() {
+  alert("ArtiZenLogo");
+});
+
+var AllProd = document.getElementById("AllProd");
+AllProd.addEventListener("click", function() {
+  alert("Усі");
+});
+
+var SearchIcon = document.getElementById("SearchIcon");
+SearchIcon.addEventListener("click", function() {
+  alert("Поиск");
+});
+
+var MyAccount = document.getElementById("MyAccount");
+MyAccount.addEventListener("click", function() {
+  alert("Акаунт");
+});
+
+var Bascet = document.getElementById("Bascet");
+Bascet.addEventListener("click", function() {
+  alert("Корзина");
+});
+// Кнопки под Хедером
+var HelpForClient = document.getElementById("HelpForClient");
+HelpForClient.addEventListener("click", function() {
+  alert("Обслуговування");
+});
+
+var PresentCard = document.getElementById("PresentCard");
+PresentCard.addEventListener("click", function() {
+  alert("Подарункові картки");
+});
+
+var Blog = document.getElementById("Blog");
+Blog.addEventListener("click", function() {
+  alert("Блог");
+});
+
+var Support = document.getElementById("Support");
+Support.addEventListener("click", function() {
+  alert("Підтримка");
+});
+
+var Sales = document.getElementById("Sales");
+Sales.addEventListener("click", function() {
+  alert("Продавати на сайті");
+});
+
+// Фрейм
+
+var Items = document.getElementById("Items");
+Items.addEventListener("click", function() {
+  alert("Одяг та взуття");
+});
+
+var Decorations = document.getElementById("Decorations");
+Decorations.addEventListener("click", function() {
+  alert("Прикраси");
+});
+
+var BeautyandHealth = document.getElementById("BeautyandHealth");
+BeautyandHealth.addEventListener("click", function() {
+  alert("Краса та здоров’я");
+});
+
+var TextileProducts = document.getElementById("TextileProducts");
+TextileProducts.addEventListener("click", function() {
+  alert("Текстильні вироби");
+});
+
+var Dishes = document.getElementById("Dishes");
+Dishes.addEventListener("click", function() {
+  alert("Посуд");
+});
+
+var Office = document.getElementById("Office");
+Office.addEventListener("click", function() {
+  alert("Канцелярія");
+});
+
+var Food = document.getElementById("Food");
+Food.addEventListener("click", function() {
+  alert("Продукти харчування та напої");
+});
+
+var GoodsForPets = document.getElementById("GoodsForPets");
+GoodsForPets.addEventListener("click", function() {
+  alert("Товари для тварин");
+});
+
+
+
+
+
+
+
+
+
+
+
